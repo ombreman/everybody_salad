2280d90182461ba3a9925069e4345b8c33e6ca8a
Changgil Jeong <ombreman21@gmail.com>
2021-06-09 15:17:25 +0100

    크롤링 불러오기

--- NAVER salad crawling.py
+++ NAVER salad crawling.py
@@ -12,28 +12,26 @@ db = client.dbeverybody_salad
 
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://terms.naver.com/list.naver?cid=48166&categoryId=48166',headers=headers)
 
-soup = BeautifulSoup(data.text, 'html.parser')
+for i in range(3):
+    j = f'https://terms.naver.com/list.naver?cid=48166&categoryId=48166&page={i+1}';
+    data = requests.get(j, headers=headers)
 
+    soup = BeautifulSoup(data.text, 'html.parser')
 
-trs = soup.select('#content > div.list_wrap > ul > li')
-for tr in trs :
-    name = tr.select_one('div.info_area > div.subject > strong > a').text.split('만드는 법')[0]
-    desc = tr.select_one('div.info_area > p').text.lstrip()
-    thumb = tr.select_one('div.thumb_area > div.thumb > a > img')['data-src']
-    doc = {
-        'salad name': name,
-        'salad desc': desc,
-        'salad thumb': thumb
+    trs = soup.select('#content > div.list_wrap > ul > li')
+    for tr in trs:
+        name = tr.select_one('div.info_area > div.subject > strong > a').text.split('만드는 법')[0]
+        desc = tr.select_one('div.info_area > p').text.lstrip()
+        thumb = tr.select_one('div.thumb_area > div.thumb > a > img')['data-src']
+        doc = {
+            'salad name': name,
+            'salad desc': desc,
+            'salad thumb': thumb
+        }
+        db.salad.insert_one(doc)
 
-    }
-    db.salad.insert_one(doc)
-    # print(thumb)
-
-
-
-
+# https://terms.naver.com/list.naver?cid=48166&categoryId=48166&page=1
 
 #content > div.list_wrap > ul > li:nth-child(1) > div.thumb_area > div.thumb.id1988477 > a:nth-child(1) > img
 #content > div.list_wrap > ul > li > div.thumb_area > div.thumb > a > img
